a0e3a1c98e2e4882437f1bb570ccd7476705c8d2
Anas El Houssaini <elhoussainianas8@gmail.com>
2026-05-29 16:42:34 -0400

    Update DriftQL project website

--- static/js/index.js
+++ static/js/index.js
@@ -6,41 +6,4 @@ $(document).ready(function() {
     $(".navbar-menu").toggleClass("is-active");
   });
 
-  const loops = Array.from(document.querySelectorAll(".sync-loop"));
-
-  function syncLoops() {
-    if (!loops.length) return;
-
-    Promise.all(
-      loops.map((video) =>
-        video.readyState >= 2
-          ? Promise.resolve()
-          : new Promise((resolve) => {
-              video.addEventListener("canplay", resolve, { once: true });
-            })
-      )
-    ).then(() => {
-      loops.forEach((video) => {
-        video.pause();
-        video.currentTime = 0;
-      });
-
-      requestAnimationFrame(() => {
-        loops.forEach((video) => {
-          const playPromise = video.play();
-          if (playPromise && typeof playPromise.catch === "function") {
-            playPromise.catch(() => {});
-          }
-        });
-      });
-    });
-  }
-
-  syncLoops();
-
-  document.addEventListener("visibilitychange", function() {
-    if (!document.hidden) {
-      syncLoops();
-    }
-  });
 });
